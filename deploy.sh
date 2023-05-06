a52b547bc83b58dccf9551b052ef55e13a00dcf2
AlanSun <alansun2020@126.com>
2023-05-06 14:34:02 -0700

    basic setup

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Build the project
+cd ~/Projects/TritonLink/tritonlink
+mvn clean package
+
+sleep 2
+
+# Copy the war file to the tomcat server
+cp target/tritonlink.war ~/Downloads/apache-tomcat-10.1.8/webapps
+
+sleep 1
+
+# Start the tomcat server
+$CATALINA_HOME/bin/startup.sh
+
+# sleep 1
+
+# # Open the local server
+# open http://localhost:8080/tritonlink/
